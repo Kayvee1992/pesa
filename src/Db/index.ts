0c166b436cc4b473c8f022ca9a48d116ebc370a4
keerthi-keyvalue <keerthi@keyvalue.systems>
2022-08-26 22:51:27 +0530

    Added DB connection

---
+++ src/Db/index.ts
@@ -0,0 +1,30 @@
+import { Module } from "@nestjs/common";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+import { TypeOrmModule } from "@nestjs/typeorm";
+import { SnakeNamingStrategy } from "typeorm-naming-strategies";
+
+@Module({
+    imports: [
+      TypeOrmModule.forRootAsync({
+        imports: [ConfigModule],
+        inject: [ConfigService],
+        useFactory: (configService: ConfigService) => ({
+          type: 'postgres',
+          host: configService.get('DB_HOST'),
+          port: configService.get('DB_PORT'),
+          username: configService.get('DB_USER'),
+          password: configService.get('DB_PASSWORD'),
+          database: configService.get('DB_SCHEMA_NAME'),
+          entities: [
+            __dirname + '/../**/*.entity.ts',
+            __dirname + '/../**/*.entity.js',
+          ],
+          synchronize: false,
+          logging: ['error', 'info', 'query'],
+          namingStrategy: new SnakeNamingStrategy(),
+        }),
+      }),
+    ],
+  })
+  export class DatabaseModule {}
+  
